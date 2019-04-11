573efcd06821a2e8f626cddf072792ce55fa2636
mrlancelot <pridhvi.uchiha@gmail.com>
2019-04-11 14:42:52 +0530

    initial commit

---
+++ parallax-template/js/init.js
@@ -0,0 +1,9 @@
+function a(){
+    document.getElementById("Second").style.display="block";
+    document.getElementById("Start").style.display="none";
+}
+function b(){
+    
+    document.getElementById("Filled").style.display="block";
+    document.getElementById("Second").style.display="none";
+}
---
+++ secondary/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/thejamespower/pen/OVNYLL.
+
+ Snazzy CSS hover states for a button.
